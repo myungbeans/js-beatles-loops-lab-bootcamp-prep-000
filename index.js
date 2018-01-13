e0e0c52d4481ba61bcf00ae4854a0a0a9381c9b1
Michael Myung <michael.h.myung@gmail.com>
2018-01-13 02:11:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,9 @@ function theBeatlesPlay(musician, instrument){
   //This string should be added to the empty array you created.
   //The loop should make the same sentence for every member of the musicians array.
   //The function should return the array of new strings.
+
+function johnLennonFacts(array){
+  
+}
+  
+}
